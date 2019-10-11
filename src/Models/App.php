8e3eac44f7cb70c9c88fcb456fb4439af7fc429c
Phu Luong <phult.contact@gmail.com>
2019-10-11 15:21:15 +0700

    Fix: add App model

---
+++ src/Models/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Megaads\Clara\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class App extends Model
+{
+    protected $table = 'sb_app';
+    protected $primaryKey = 'id';
+}
